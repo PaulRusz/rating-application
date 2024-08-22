f872e23f186b69f80c27a3b5ac63635a19f37574
PaulRusz <paulruszkay@gmail.com>
2024-08-22 16:40:09 -0400

    folders created & code within App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/navbar.js";
+import Footer from "./components/footer.js";
+import Homepage from "./pages/homepage.js";
+import Login from "./pages/login.js";
+import Profile from "./pages/profile.js";
+import styles from "./styles/global.module.scss";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className={styles.App}>
+        <Switch>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/login" component={Login} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
   );
 }
-
-export default App;
