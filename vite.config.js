818c026a21ed84c76d968b7455b7062804dd57f8
PaulRusz <paulruszkay@gmail.com>
2024-09-17 15:03:25 -0400

    client dist in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   root: "client",
   plugins: [react()],
   build: {
-    outDir: "./client/client/dist",
+    outDir: "./client/dist",
   },
   server: {
     port: 3000,
