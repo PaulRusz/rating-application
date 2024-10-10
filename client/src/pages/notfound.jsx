cbd6d209c85c1c7dd59d64aa78e767b82f043b79
PaulRusz <paulruszkay@gmail.com>
2024-10-10 16:31:30 -0400

    rating changes, slight global scss changes

--- client/src/pages/notfound.jsx
+++ client/src/pages/notfound.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import styles from "../styles/notfound.module.scss";
 
-export default function NotFound() {
+export default function NotFoundPage() {
   return (
     <div className={styles.notFoundContainer}>
-      <h1>Welcome to the not found page!</h1>
-      <p>
-        On this page a user will be able to see that an item hasnt been found
-        during a search.
-      </p>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <section className={styles.cta}>
+        <h2>What would you like to do next?</h2>
+        <Link to="/" className={styles.ctaButton}>
+          Go to Homepage
+        </Link>
+      </section>
     </div>
   );
 }
