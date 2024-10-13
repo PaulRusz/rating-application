82147372e4255bcf46f329f88fc64633d47f66f8
PaulRusz <paulruszkay@gmail.com>
2024-10-13 10:59:30 -0400

    nevermind that didn't work.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
       <div className={styles.App}>
         <Routes>
           <Route
-            path="/homepage"
+            path="/"
             element={
               <Navigate to={isLoggedIn ? "/homepage" : "/welcomePage"} />
             }
