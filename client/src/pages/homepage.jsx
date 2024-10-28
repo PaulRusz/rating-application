7f7e2d4a7c0171e8c630e8dce90eb2eff4321ba6
PaulRusz <paulruszkay@gmail.com>
2024-10-28 14:14:46 -0400

    changed import line to work

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import AuthService from "./auth";
+import AuthService from ".././utils/auth";
 import { Link } from "react-router-dom";
 import styles from "../styles/homepage.module.scss";
 
